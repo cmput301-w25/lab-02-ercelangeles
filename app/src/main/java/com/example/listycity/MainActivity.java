bfb9d38eb1931aa31229acc765ffa676a308959d
ercelangeles <156619970+ercelangeles@users.noreply.github.com>
2025-01-15 19:16:32 -0700

    Added some code

--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.listycity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        cityList = findViewById(R.id.city_list);
+        cityList = findViewById(R.id.city_list);    // Referenced in activity_main.xml
 
         String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
 
@@ -37,9 +38,20 @@ public class MainActivity extends AppCompatActivity {
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
+        // Add "add_city" and "delete_city", then when you tpa them a "confirm_city" pops up to add or delete a city.
+        // Use set On Listeners to enable and disable the popup window and for button functionality
+        // Add the buttonson java (or whatever's easier.
+        // Edit text
+        // Don't define things in xml
+        // Use XML for the visuals, use Java for the logic.
+            // Add the buttons in Java, then
+
+        Button addCity = findViewById(R.id.btn_addCity);    // Anything inside the brackets are integers.
 
 
 
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
