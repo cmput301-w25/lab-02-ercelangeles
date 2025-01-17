c4e5ee25e54c1943fb339a23bfa05a51de317b36
ercelangeles <156619970+ercelangeles@users.noreply.github.com>
2025-01-17 10:34:08 -0700

    Added new lab 2 android studio project

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,10 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    // Declare variables to be referenced later.
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
@@ -28,34 +26,45 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        cityList = findViewById(R.id.city_list);    // Referenced in activity_main.xml
+        cityList = findViewById(R.id.city_list);
 
         String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
-
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
-        // Add "add_city" and "delete_city", then when you tpa them a "confirm_city" pops up to add or delete a city.
-        // Use set On Listeners to enable and disable the popup window and for button functionality
-        // Add the buttonson java (or whatever's easier.
-        // Edit text
-        // Don't define things in xml
-        // Use XML for the visuals, use Java for the logic.
-            // Add the buttons in Java, then
+        view.findViewById(R.id.Add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        Button addCity = findViewById(R.id.btn_addCity);    // Anything inside the brackets are integers.
+            }
+        });
+
+        view.findViewById(R.id.Delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
+        /*
+        view.findViewById(R.id.Confirm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
-        }
+            }
+        });
+         */
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+
+
 }
