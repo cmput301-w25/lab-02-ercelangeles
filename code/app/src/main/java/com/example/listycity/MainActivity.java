8892f90f4aabc0026d134b5c2792bec5c8f1a150
ercelangeles <156619970+ercelangeles@users.noreply.github.com>
2025-01-17 13:32:02 -0700

    Modified activity_main.xml, MainActivity.java
    activity_main.xml: changed layout of "ADD CITY" and "DELETE CITY" buttons.
    
    MainActivity.java: added logic for "ADD CITY", "DELETE CITY" and "CONFIRM" buttons, as well as the text box.

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.listycity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    /* Taken from https://www.youtube.com/watch?v=-Yelywy0idc
+    * Authored by: MISSIONTOP Tech
+    * Taken by: Ercel Angeles
+    * Taken on: January 17, 2025*/
+    EditText editText;
+    Button addButton;
+    Button deleteButton;
+    Button confirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,35 +45,53 @@ public class MainActivity extends AppCompatActivity {
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
-        view.findViewById(R.id.Add).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        editText = findViewById(R.id.textBox);
+        editText.setVisibility(View.INVISIBLE);
 
+        addButton = findViewById(R.id.Add);
+
+        confirmButton = findViewById(R.id.CONFIRM);
+        confirmButton.setVisibility(View.INVISIBLE);
+
+        // When "ADD CITY" button is clicked, a textbox and a "CONFIRM" button appears at the bottom of the screen.
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            /* [3] Function code is taken from https://www.youtube.com/watch?v=fqfv6gOZZhY
+            * Authored by: Teach Programming
+            * Taken by: Ercel Angeles
+            * Taken on: January 17, 2025 */
+            public void onClick(View v) {
+                if (editText.getVisibility() == View.INVISIBLE)
+                    editText.setVisibility(View.VISIBLE);
+                if (confirmButton.getVisibility() == View.INVISIBLE)
+                    confirmButton.setVisibility(View.VISIBLE);
             }
         });
 
-        view.findViewById(R.id.Delete).setOnClickListener(new View.OnClickListener() {
+        // When "CONFIRM" button is clicked, textbox and confirm button disappear.
+        confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View v) {
+                /* [4] Taken from https://www.youtube.com/watch?v=7yZngVxaZDM
+                * Authored by: IJ Apps
+                * Taken by: Ercel Angeles
+                * Taken on: January 17, 2025 */
+                String userInput = editText.getText().toString();   // [4]
+                dataList.add(userInput);    // [4]
+                cityList.setAdapter(cityAdapter);   // [4]
+                editText.setVisibility(View.INVISIBLE);
+                confirmButton.setVisibility(View.INVISIBLE);
             }
         });
 
-
-        /*
-        view.findViewById(R.id.Confirm).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-         */
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+
 
 
 
